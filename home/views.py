f9eb8947e237fda3a68ad85de754de5f7c7ab481
jbgonca <you@example.com>
2023-09-11 11:46:14 -0300

    meu terceiro commit

--- home/views.py
+++ home/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+# def index(request):
+#     context = {
+#         'disciplina': 'Desenvolvimento Web',
+#         'tecnologia': 'Python e Django'
+#     }
+# return render(request,'index.html', context)
+class IndexView(TemplateView):
+    template_name = 'index.html'
