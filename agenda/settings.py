42f30c7a1ddafec597256447be38b25bc0778e04
jbgonca <you@example.com>
2023-10-02 09:24:23 -0300

    mudanças

--- agenda/settings.py
+++ agenda/settings.py
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
-INSTALLED_APPS += ['home', 'produto',]
+INSTALLED_APPS += ['stdimage',]
+
+INSTALLED_APPS += ['home', 'produto', 'cliente']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -118,12 +120,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL= '/media/'
 
 STATICFILES_DIRS= [
     BASE_DIR / "static",
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- home/models.py
+++ home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from stdimage import StdImageField
 
-# Create your models here.
+class Pessoa(models.Model):
+    nome = models.CharField('Nome', max_length=50, help_text='Nome completo')
+    fone = models.CharField('Telefone', max_length=15, help_text='Número do telefone')
+    email = models.EmailField('E-mail', max_length=100, help_text='Endereço de E-mail', unique=True)
+    foto = StdImageField('Foto', upload_to='pessoas', variations=
+    {'thumbnail': {'width': 100, 'height': 100, 'crop': True}}, delete_orphans=True, null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.nome
+
