e886d3a7296149f04911b38cb307532a7c04ca7b
jbgonca <you@example.com>
2023-10-16 10:05:48 -0300

    DIA CHUVOSO

--- agenda/urls.py
+++ agenda/urls.py
@@ -19,10 +19,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 admin.site.site_header = "Agendamento de Horários"
 admin.site.index_title = "LavaCar - Agendamento de Horários"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls'))
+    path('', include('home.urls')),
+    path('', include('cliente.urls')),
 ]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- home/views.py
+++ home/views.py
@@ -1,11 +1,33 @@
-#from django.shortcuts import render
 from django.views.generic import TemplateView
+from cliente.models import Cliente
+from funcionario.models import Funcionario
+from servico.models import Servico
+from produto.models import Produto
+from atendimento.models import Atendimento
 
-# def index(request):
-#     context = {
-#         'disciplina': 'Desenvolvimento Web',
-#         'tecnologia': 'Python e Django'
-#     }
-# return render(request,'index.html', context)
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['qtd_clientes'] = Cliente.objects.count()
+        context['qtd_funcionarios'] = Funcionario.objects.count()
+        context['qtd_servicos'] = Servico.objects.count()
+        context['qtd_produtos'] = Produto.objects.count()
+        context['qtd_agendamentos'] = Atendimento.objects.count()
+        return context
+
+
+
+
+# #from django.shortcuts import render
+# from django.views.generic import TemplateView
+#
+# # def index(request):
+# #     context = {
+# #         'disciplina': 'Desenvolvimento Web',
+# #         'tecnologia': 'Python e Django'
+# #     }
+# # return render(request,'index.html', context)
+# class IndexView(TemplateView):
+#     template_name = 'index.html'
